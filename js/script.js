0c347e04ee4f9ad1b8a92354ce975d82df6f8769
Cédric Jeanneret <tengu@ethack.org>
2015-02-15 17:58:10 +0100

    updated website

--- js/script.js
+++ js/script.js
@@ -4,42 +4,48 @@ $.getJSON('/jsons/index.json', function(data) {
   $('#update').html(latest);
 
   $.getJSON("/jsons/"+latest+".json", function(data) {
-    var sites = Object.keys(data);
+    var sites = Object.keys(data['banks']);
     var data_set = new Array();
     sites.sort();
     var average_front = 0;
     var average_ebanking = 0;
+    var max_grade = 0;
     $.each(sites, function(s) {
-      site = data[sites[s]];
 
-      if (site['role'] == 'front') {
+      var site = data['banks'][sites[s]];
+      var front = site['frontend'];
+      var back  = site['backend'];
 
-        if (site['ebanking'] != 'app' && site['ebanking'] != 'self') {
-          data_set.push({url : sites[s], name: site['bank_name'], hash: MD5(sites[s])});
-          var ebanking = data[site['ebanking']];
-          build_row(site, sites[s], ebanking);
-          
-          average_front += site['evaluation']['result'];
-          average_ebanking += ebanking['evaluation']['result'];
-
-        } else if(site['ebanking'] == 'self') {
-          data_set.push({url : sites[s], name: site['bank_name'], hash: MD5(sites[s])});
-          ebanking = site;
-          ebanking['role'] = 'ebanking';
-          build_row(site, sites[s], ebanking);
-          
-          average_front += site['evaluation']['result'];
-          average_ebanking += ebanking['evaluation']['result'];
-
-        } else if (site['ebanking'] == 'app') {
-          //build_row(site, sites[s], {});
+      if (site['backend'] != 'app' && site['backend'] != 'self') {
+        data_set.push({name: sites[s], hash: MD5(sites[s])});
+        build_row(site, sites[s]);
+        
+        if (site['results'][front] != undefined) {
+          average_front += site['results'][front]['grades']['total'];
+          max_grade = site['results'][front]['grades']['max'];
         }
+        if (site['results'][back] != undefined) {
+          average_ebanking += site['results'][back]['grades']['total'];
+        }
+
+      } else if(site['backend'] == 'self') {
+        data_set.push({name: sites[s], hash: MD5(sites[s])});
+        build_row(site, sites[s]);
+ 
+        if (site['results'][front] != undefined) {
+          average_front += site['results'][front]['grades']['total'];
+          average_ebanking += site['results'][front]['grades']['total'];
+        }
+
+      } else if (site['backend'] == 'app') {
+        //build_row(site, sites[s], {});
       }
     });
 
     $('#bankers').html(data_set.length);
     $('#average_front').html((average_front/data_set.length).toFixed(2));
     $('#average_ebanking').html((average_ebanking/data_set.length).toFixed(2));
+    $('#max_grade').html(max_grade);
 
     var search = function(strs) {
       return function findMatches(q, cb) {
@@ -90,35 +96,77 @@ $.getJSON('/jsons/index.json', function(data) {
     });
 
   }).done(function() { $('#loading').hide(); });
-}).fail(function() {$('#loading').hide();});
+}).fail(function() {
+  $('#loading').hide();
+  console.log('Error while loading json');
+});
 
-function build_row(site, url, ebanking) {
+function build_row(site, bank_name) {
 
-  var evaluation = site['evaluation'];
-  // get results from JSON
-  site_result = evaluation['result'];
-  max_result  = evaluation['max_result'];
+  frontend = site['frontend'];
+  backend  = site['backend'];
+  id       = MD5(bank_name);
+  var eval_front;
+  var eval_back
 
-  id = MD5(url);
   line = '<section id="'+id+'" >';
-  line += '<h2><a name="'+id+'" href="#'+id+'"> '+site['bank_name']+'</a></h2>';
-  line += '<ul class="note"><li>'+site_result+'/'+max_result+'</li>';
-  // ebanking note
-  if (site['ebanking'] != undefined && site['ebanking'] != 'app') {
-    eb_result = ebanking['evaluation']['result'];
-    line += '<li>'+eb_result+'/'+max_result+'</li>';
+  line += '<h2><a name="'+id+'" href="#'+id+'"> '+bank_name+'</a></h2>';
+  line += '<ul class="note">';
+  // do we have some frontend results?
+  if (site['results'][frontend] != undefined) {
+    eval_front = site['results'][frontend];
+    site_result = eval_front['grades']['total'];
+    max_result  = eval_front['grades']['max'];
+
+    line += '<li>'+site_result+'/'+max_result+'</li>';
   } else {
     line += '<li>—</li>';
+  }
+  // ebanking note
+  if (site['results'][backend] != undefined && backend != 'app') {
+    eval_back = site['results'][backend];
+    eb_result  = eval_back['grades']['total'];
+    max_result = eval_back['grades']['max'];
+    line += '<li>'+eb_result+'/'+max_result+'</li>';
+  } else if(backend == 'self') {
+    eval_front = site['results'][frontend];
+    if (eval_front != undefined) {
+      site_result = eval_front['grades']['total'];
+      max_result  = eval_front['grades']['max'];
+      line += '<li>'+site_result+'/'+max_result+'</li>';
+    } else {
+      line += '<li>—</li>';
+      console.log(bank_name);
+    }
+  } else {
+    line += '<li>—</li>';
+    console.log(bank_name);
   }
   line += '</ul>';
   line += '<ul class="bloc left">';
   // front
-  line += build_tile(evaluation, url);
+  if (site['results'][frontend] != undefined) {
+    line += build_tile(eval_front, frontend);
+  } else {
+    line += '<li><p>'+frontend+'</p></li>';
+    line += '<li><p>—</p></li>';
+    line += '<li><p>Non</p></li>';
+    line += '<li><p>—</p></li>';
+    line += '<li><p>—</p></li>';
+    line += '<li><p>—</p></li>';
+    line += '<li><p>—</p></li>';
+    line += '<li><p>—</p></li>';
+    line += '<li><p>—</p></li>';
+    line += '<li><p>—</p></li>';
+    line += '<li><p>—</p></li>';
+  }
   line += '</ul>';
+  // ebanking results
   line += '<ul class="bloc right">';
-  if (site['ebanking'] != undefined && site['ebanking'] != 'app') {
-    // ebanking results
-    line += build_tile(ebanking['evaluation'], site['ebanking']);
+  if (site['results'][backend] != undefined && backend != 'app') {
+    line += build_tile(eval_back, backend);
+  } else if (backend == 'self') {
+    line += build_tile(eval_front, frontend);
   } else {
     line += '<li><p>— Aucun —</p></li>';
     line += '<li><p>—</p></li>';
@@ -134,14 +182,7 @@ function build_row(site, url, ebanking) {
   }
   line += '</ul>';
   line += '<div class="postContent hidding" xattr="'+id+'">';
-  line += '<ul class="bloc left"><pre>';
   line += build_extended(site);
-  line += '</pre></ul>';
-  line += '<ul class="bloc right"><pre>';
-  if (site['ebanking'] != undefined && site['ebanking'] != 'app') {
-    line += build_extended(ebanking);
-  }
-  line += '</pre></ul>';
   line += '</div>';
   line += '<button class="more" title="more" xattr="'+id+'"></button>';
   line += '<button class="more" title="less" xattr="'+id+'"></button>';
@@ -156,54 +197,78 @@ function build_tile(evaluation, url) {
   } else {
     line = '<li><p>'+url+'</p></li>';
   }
-  if (evaluation['detail']['country'] != undefined) {
-    line += '<li><p>'+evaluation['detail']['country']['expl']+'</p></li>';
-  } else {
-    line += '<li><p>—</p></li>';
-  }
 
-  var translation = {
-    absent: 'absent',
-    forced: 'forcé',
-    optional: 'facultatif'
-  };
+  if (evaluation != undefined) {
+    line += '<li><p>'+evaluation.country.ip+'</p></li>';
 
-
-
-  line += '<li><p>'+translation[evaluation['detail']['ssl']['expl']]+'</p></li>';
-  if (evaluation['detail']['ssl']['expl'] != 'absent') {
-    end = new Date(evaluation['detail']['cert']['expl']);
-    line += '<li><p>'+end.getDate()+'.'+end.getMonth()+'.'+end.getFullYear()+'</p></li>';
-  } else {
-    line += '<li><p>—</p></li>';
-  }
-  line += '<li><p>'+evaluation['detail']['server']['expl']+'</p></li>';
-  line += '<li><p>'+evaluation['detail']['protocols']['expl'].join(', ')+'</p></li>';
-
-  strong_percent = parseFloat(evaluation['detail']['ciphers']['expl']['strong']).toFixed(1);
-  weak_percent   = parseFloat(evaluation['detail']['ciphers']['expl']['weak']).toFixed(1);
-  line += '<li><p>'+strong_percent+'% forts, '+weak_percent+'% faibles</p></li>';
-
-  strong_percent = parseFloat(evaluation['detail']['pfs']['strong']).toFixed(1);
-  weak_percent   = parseFloat(evaluation['detail']['pfs']['weak']).toFixed(1);
-  line += '<li><p>'+strong_percent+'% forts, '+weak_percent+'% faibles</p></li>';
-  if (evaluation['detail']['trackers']['expl'] != null) {
-    line += '<li><p>'+evaluation['detail']['trackers']['expl'].join(', ')+'</p></li>';
-  } else {
-    line += '<li><p>—</p></li>';
-  }
-  if (evaluation['detail']['flash']['points'] != 0) {
-    line += '<li><p>Oui</p></li>';
-  } else {
-    line += '<li><p>—</p></li>';
-  }
-  if (evaluation['detail']['frames']['expl'] == 'yes' ) {
-    if (evaluation['detail']['frames']['points'] == 0) {
-      line += '<li><p>Oui, protégées</p></li>';
+    if (evaluation['key'] != undefined) {
+      if (evaluation['response']['force_ssl'] == 1) {
+        line += '<li><p>Oui, forcé</p></li>';
+      } else if(evaluation['response']['force_ssl'] == 2) {
+        line += '<li><p>Oui, uniquement</p></li>';
+      } else {
+        line += '<li><p>Oui, facultatif</p></li>';
+      }
     } else {
-      line += '<li><p>Oui, non protégées</p></li>';
+      line += '<li><p>Absent</p></li>';
+    }
+    if (evaluation['key'] != undefined) {
+      end = new Date(evaluation['key']['notAfter']);
+      line += '<li><p>'+end.getDate()+'.'+end.getMonth()+'.'+end.getFullYear()+'</p></li>';
+    } else {
+      line += '<li><p>—</p></li>';
+    }
+    if (evaluation.key.ocspStapling == 0) {
+      line += '<li><p>Non</p></li>';
+    } else {
+      line += '<li><p>Oui</p></li>';
+    }
+    var proto = evaluation['protocols']
+    line += '<li><p>'+evaluation['protocols']['enabled'].join(', ')+'</p></li>';
+
+    var preferred = new Array();
+    $.each(evaluation['preferredCiphers'], function(proto, data) {
+      if (data['name'] != null) {
+        preferred.push(data['name']);
+      }
+    });
+    line += '<li><p></p><ul><li>'+preferred.join('</li><li>')+'</li></ul></li>';
+    if (evaluation.response.hsts != '') {
+      line += '<li><p>Oui ('+evaluation.response.hsts+')</p></li>';
+    } else {
+      line += '<li><p>Non</p></li>';
+    }
+
+    if (evaluation['trackers'].length != 0) {
+      line += '<li><p>'+evaluation['trackers'].join(', ')+'</p></li>';
+    } else {
+      line += '<li><p>—</p></li>';
+    }
+    if (evaluation['response']['flash'] != 0) {
+      line += '<li><p>Oui</p></li>';
+    } else {
+      line += '<li><p>—</p></li>';
+    }
+    if (evaluation['response']['frame'] == 1 ) {
+      if (evaluation['response']['xframeopt'] != '') {
+        line += '<li><p>Oui, protégées</p></li>';
+      } else {
+        line += '<li><p>Oui, non protégées</p></li>';
+      }
+    } else {
+      line += '<li><p>—</p></li>';
     }
   } else {
+    console.log(url);
+    line += '<li><p>—</p></li>';
+    line += '<li><p>Non</p></li>';
+    line += '<li><p>—</p></li>';
+    line += '<li><p>—</p></li>';
+    line += '<li><p>—</p></li>';
+    line += '<li><p>—</p></li>';
+    line += '<li><p>—</p></li>';
+    line += '<li><p>—</p></li>';
+    line += '<li><p>—</p></li>';
     line += '<li><p>—</p></li>';
   }
   return line;
@@ -211,149 +276,59 @@ function build_tile(evaluation, url) {
 
 function build_extended(site) {
 
-  var ssl_verification = {
-    '0':  'ok',
-    '2':  "Impossible d'obtenir l'émetteur du certificat",
-    '3':  "Impossible d'obtenir le CRL",
-    '4':  "Impossible de déchiffrer la signature du certificat",
-    '5':  "Impossible de déchiffrer la signature du CRL",
-    '6':  "Impossible de déchiffrer la clef publique de l'émetteur",
-    '7':  "Erreur de vérification de la signature du certificat",
-    '8':  "Erreur de vérification de la signature du CRL",
-    '9':  "Le certificat n'est pas encore valide",
-    '10': "Le certificat est expiré",
-    '11': "CRL pas encore valide",
-    '12': "CRL expiré",
-    '13': "Erreur de format de date (notBefore) (certificat)",
-    '14': "Erreur de format de date (notAfter) (certificat)",
-    '15': "Erreur de format de date (lastUpdate) (CRL)",
-    '16': "Erreur de format de date (nextUpdate) (CRL)",
-    '17': "Out of memory",
-    '18': "Certificat auto-signé",
-    '19': "Certificat auto-signé dans la chaîne de certificats",
-    '20': "Impossible d'obtenir le certificat de l'émetteur local",
-    '21': "Impossible de vérifier le premier certificat",
-    '22': "Chaîne de certificat trop longue",
-    '23': "Certificat révoqué",
-    '24': "Certificat CA invalide",
-    '25': "path length constraint exceeded",
-    '26': "unsupported certificate purpose",
-    '27': "Certificat non approuvé",
-    '28': "Certificat rejeté",
-    '29': "Incompatibilité de sujet émetteur",
-    '30': "authority and subject key identifier mismatch",
-    '31': "authority and issuer serial number mismatch",
-    '32': "key usage does not include certificate signing",
-    '50': "application verification failure"
+  var frontend = site['frontend'];
+  var backend  = site['backend'];
+
+  var front_tile;
+
+  line  = '<ul class="bloc left"><pre>';
+  // front
+  if (site['results'][frontend] != undefined) {
+    front_tile = build_extra_tile(site['results'][frontend])
+  } else {
+    front_tile = 'Aucune information';
   }
+  line += front_tile;
+  line += '</pre></ul>';
+  line += '<ul class="bloc right"><pre>';
+  if (site['results'][backend] != undefined && site['results'][backend] != 'app') {
+    // backend
+    line += build_extra_tile(site['results'][backend]);
+  } else if (backend != undefined && backend == 'self') {
+    line += front_tile;
+  } else {
+    line += 'Aucune information';
+  }
+  line += '</pre></ul>';
 
+  return line;
+}
 
-
-  evaluation = site['evaluation'];
-
-  var line = '<li>Détail de la note<ul>';
+function build_extra_tile(site) {
+  var grades = site.grades;
 
   var type = {
     'id-ecPublicKey': 'ECC',
-    'rsaEncryption': 'RSA'
+    'rsaEncryption': 'RSA',
+    'sha256WithRSAEncryption': 'SHA2',
+    'sha1WithRSAEncryption': 'SHA1'
   };
-  if (evaluation['detail']['cert']['sign_algo'] != undefined) {
 
-    line += '<li>Certificat ('+ type[evaluation['detail']['cert']['key_algo']] +') : ';
-    line += (
-        evaluation['detail']['cert']['points'] +
-        evaluation['detail']['cert']['sign_algo'] +
-        evaluation['detail']['cert']['key_size']
-        );
-  } else {
-    line += '<li>Certificat ('+ type[site['certificate']['key_algo']] +') : '+evaluation['detail']['cert']['points'];
-  }
-
-  if (
-      site['certificate'] != undefined &&
-      site['certificate']['verify'] != undefined &&
-      site['certificate']['verify'] != 0
-      ) {
-    var i = site['certificate']['verify'];
-    line += ' '+ssl_verification[i];
-  }
-  line += '</li>';
-  if (evaluation['detail']['ciphers']['malus'] != undefined) {
-    line += '<li>Ciphers : '+evaluation['detail']['ciphers']['points'] + evaluation['detail']['ciphers']['malus'];
-    line += ' (malus: '+evaluation['detail']['ciphers']['malus']+')</li>';
-  } else {
-    line += '<li>Ciphers : '+evaluation['detail']['ciphers']['points']+'</li>';
-  }
-  line += '<li>Pays : '+evaluation['detail']['country']['points']+'</li>';
-  line += '<li>Flash : '+evaluation['detail']['flash']['points']+'</li>';
-  line += '<li>Frames : '+evaluation['detail']['frames']['points']+'</li>';
-  line += '<li>PFS : '+evaluation['detail']['pfs']['points']+'</li>';
-  line += '<li>Protocoles : '+evaluation['detail']['protocols']['points']+'</li>';
-  line += '<li>SSL : '+evaluation['detail']['ssl']['points']+'</li>';
-  line += '<li>Trackers : '+evaluation['detail']['trackers']['points']+'</li>';
-
-  line += '</ul></li>';
-
-
-  line += '<li>Ciphers supportés : <br>';
-  var protos = Object.keys(evaluation['detail']['ciphers']['weak']);
-  protos.sort();
-  $.each(protos, function(k) {
-    proto = protos[k];
-    ciphers = evaluation['detail']['ciphers']['weak'][proto];
-    line += proto+' (faibles)<ul>';
-    $.each(ciphers, function(hash) {
-      var pfs = 'avec PFS';
-      if (ciphers[hash]['pfs'] == 'no_pfs') {
-        pfs = 'sans PFS';
-      }
-      line += '<li>'+ciphers[hash]['cipher']+' <small><i>'+pfs+'</i></small></li>';
-    })
-    line += '</ul>';
-  });
-
-  protos = Object.keys(evaluation['detail']['ciphers']['strong']);
-  protos.sort();
-  $.each(protos, function(k) {
-    proto = protos[k];
-    ciphers = evaluation['detail']['ciphers']['strong'][proto];
-    line += proto+' (forts)<ul>';
-    $.each(ciphers, function(hash) {
-      var pfs = 'avec PFS';
-      if (ciphers[hash]['pfs'] == 'no_pfs') {
-        pfs = 'sans PFS';
-      }
-      line += '<li>'+ciphers[hash]['cipher']+' <small><i>'+pfs+'</i></small></li>';
-    })
-    line += '</ul>';
-  });
-
-  line += 'Informations IP:<ul>';
+  var line = '<li>Détail de la note<ul>';
+  line += '<li>Certificat (commonName) : '+grades.cert_match+'</li>';
+  line += '<li>Certificat (échéance) : '+grades.cert_validity+'</li>';
+  line += '<li>Ciphers : '+grades.ciphers+'</li>';
+  line += '<li>Flash : '+grades.flash+'</li>';
+  line += '<li>Frame : '+grades.frames+'</li>';
+  line += '<li>HSTS : '+grades.hsts+'</li>';
+  line += '<li>OCSP Stapling : '+grades.ocsp_stapling+'</li>';
+  line += '<li>Protocoles : '+grades.protocols+'</li>';
+  line += '<li>Redirection SSL : '+grades.enforce_ssl+'</li>';
+  line += '<li>Signature : '+grades.signature+' ('+ type[site.key.signatureAlgorithm] +')</li>';
+  line += '<li>Taille de la clef : '+grades.keysize+' ('+site.key.keySize+')</li>';
+  line += '<li>Trackers : '+grades.trackers+'</li>';
   line += '</ul>';
-  $.each(site['ips'], function(ip) {
-    line += '<li>'+ip+'<ul>';
-    ip_elements = Object.keys(site['ips'][ip]);
-    ip_elements.sort();
-    $.each(ip_elements, function(k) {
-      var el = ip_elements[k]
-      if (
-        el.toLowerCase() == 'country' ||
-        el.toLowerCase() == 'mnt-by'  ||
-        el.toLowerCase() == 'admin-c' ||
-        el.toLowerCase() == 'descr'   ||
-        el.toLowerCase() == 'netname' ||
-        el.toLowerCase() == 'role'    ||
-        el.toLowerCase() == 'org-name'||
-        el.toLowerCase() == 'org'
-        ) {
-          v = site['ips'][ip][el];
-          line += '<li>'+el+': '+v+'</li>';
-        }
-    });
-    line += '</ul></li>';
-  });
 
   line += '</li>';
-
   return line;
 }
